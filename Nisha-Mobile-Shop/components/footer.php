554e3c324abbbc72f19699dc7af3cc301e442fc8
prasad pradipkumar <97493180+Pradip7865@users.noreply.github.com>
2023-02-16 22:54:20 +0530

    Add files via upload
    this is mobile shop website fully working

---
+++ Nisha-Mobile-Shop/components/footer.php
@@ -0,0 +1,38 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+         <a href="shop.php"> <i class="fas fa-angle-right"></i> shop</a>
+         <a href="contact.php"> <i class="fas fa-angle-right"></i> contact</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="user_login.php"> <i class="fas fa-angle-right"></i> login</a>
+         <a href="user_register.php"> <i class="fas fa-angle-right"></i> register</a>
+      </div>
+
+      <div class="box">
+         <h3>contact us</h3>
+         <a href="tel:1234567890"><i class="fas fa-phone"></i> +91-9999999999</a>
+         <a href="tel:11122233333"><i class="fas fa-phone"></i> +91-9999999999</a>
+         <a href="mailto:21amtics499@gmail.com"><i class="fas fa-envelope"></i> 21amtics499@gmail.com</a>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"><i class="fab fa-facebook-f"></i>facebook</a>
+         <a href="#"><i class="fab fa-twitter"></i>twitter</a>
+         <a href="#"><i class="fab fa-instagram"></i>instagram</a>
+         <a href="#"><i class="fab fa-linkedin"></i>linkedin</a>
+      </div>
+
+   </section>
+
+   <div class="credit">copyright :- <?= date('Y'); ?> by <span>Pradip Kumar</span></div>
+
+</footer>
